086b0c115bb8e3ccc2e85f3f673367ef0d0fc281
Cooper Saye <coopersaye@gmail.com>
2021-04-22 15:04:26 -0600

    Import player movement script.

---
+++ Assets/Movement3D/Scripts/Dani/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform playerTransform;
+
+    private void Update()
+    {
+        // Move to player position
+        transform.position = playerTransform.position;
+    }
+}
